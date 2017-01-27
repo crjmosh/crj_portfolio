64ba05aa7ef0bbf169f8ad4f8757f19f0d8e291c
Cael Jensen <crjmosh@gmail.com>
2017-01-26 18:56:52 -0700

    removed content column side margins on mobile

--- app/assets/javascripts/portfolioItemStyling.js
+++ app/assets/javascripts/portfolioItemStyling.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
 	var $portfolioItems = $('.portfolio-item');
 	var $portfolioItemText = $('.port-item-text');
+	var $contentColumn = $('#content-column');
 
 	function setStyling() {
 		var winWidth = $(window).width();
 		if (winWidth <= 600) {
 			$portfolioItems.removeClass("valign-wrapper");
 			$portfolioItemText.addClass("center");
+			$contentColumn.css({padding : "0px"})
 		} else {
 			$portfolioItems.addClass("valign-wrapper")
 			$portfolioItemText.removeClass("center");
+			$contentColumn.css({padding : "0px 20px"})
 		}
 	}
 
